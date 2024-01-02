9e59e4d24dd66ee8b0edccb31c75c5b1241ffbda
Stephen123 <Stephen.Liu25@ycdsbk12.ca>
2024-01-02 18:46:43 -0500

    Added level 1 to the game that is mostly functional (Needs final adjustments)

--- Sketch2.java
+++ Sketch2.java
@@ -9,6 +9,7 @@ public class Sketch2 extends PApplet {
   PImage imgDadSmall;
   PImage imgDadMedium;
   PImage imgDadMadSmall;
+  PImage imgMutahar;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -22,28 +23,74 @@ public class Sketch2 extends PApplet {
     imgDadSmall = loadImage("Dad Game Character Small.png");
     imgDadMedium = loadImage("Dad Game Character Medium.png");
     imgDadMadSmall = loadImage("Dad Game Character Kishibe Rohan Small.png");
+    imgMutahar = loadImage("Mutahar Laugh.jpg");
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Declare variables
+  float dadX = 50;
+  float dadY = 270;
+
+  // Draw background
   public void setup() {
-    background(210, 255, 173);
+    background(3, 248, 252);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Draw background again for clean animations
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(3, 248, 252);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Call each level
+    level1();
   }
+
+  public void level1(){
+
+  // Draw side walls
+  fill(0, 0, 0);
+  rect(0, 0, 1200, 260);
+  rect(0, 400, 1200, 270);
+
+  // Draw end square with level number
+  fill(255, 34, 5);
+  rect(1085, 260, 115, 140);
   
-  // define other methods down here.
+  fill(255, 255, 255);
+  textSize(35);
+  text("Level 1", 1080, 440);
+
+  // Add level instructions
+  textSize(25);
+  text("Use WASD to control dad. (pro tip: if dad doesnt move, click the game window once ;) )", 70, 100);
+
+  // Draw dad and make dad move based on WASD presses
+    if(keyPressed){
+
+      if(key == 'w'){
+        dadY-=2;
+      }
+      else if(key == 's'){
+        dadY+=2;
+      }
+      else if(key == 'a'){
+        dadX-=2;
+      }
+      else if(key == 'd'){
+        dadX+= 2;
+      }
+    }
+    image(imgDadMedium, dadX, dadY);
+
+    // Add collision detection to walls (WORK IN PROGRESS)
+    if(dadY < 260 || dadY > 277){
+      image(imgMutahar, 200, 100);
+      textSize(30);
+      text("lol how did u even hit the wall???", 300, 600);
 }
+    // Add collision detection if player reaches red square
+    if(dadX >= 1080){
+      textSize(30);
+      text("*insert jumpscare*", 300, 600);
+  }
+}
+}
+
