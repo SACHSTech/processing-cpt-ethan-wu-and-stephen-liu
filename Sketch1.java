e74231966640e5bb3867c5961dabb16702c7ad14
Ewu52 <ethan.wu25@ycdsbk12.ca>
2024-01-08 14:25:33 -0500

    added cutscene 3, with character dying.  Added dialogue. Added many comments to describe the code

--- Sketch1.java
+++ Sketch1.java
@@ -33,6 +33,8 @@ public class Sketch1 extends PApplet {
   int text_Y1 = 100;
   int text_X2 = 600;
   int text_Y2 = 650;
+  int text_X3 = 1000;
+  int text_Y3 = 400;
 
   // Life Variables
   int numLife = 1;
@@ -69,14 +71,6 @@ public class Sketch1 extends PApplet {
 
     // Call cutscene1 Method
     cutscene1();
-
-    // Wipe Screen to get to next scene, and reset frame counter
-    if (frameCount >= 360) {
-      background(255);
-      textSize(50);
-      textAlign(CENTER, CENTER);
-      text("LEAVING THE HOUSE... BE PATIENT OK", width / 2, height / 2);
-    }
     
     // Call cutscene2 Method
     cutscene2();
@@ -116,14 +110,24 @@ public class Sketch1 extends PApplet {
       imgDad_X += 5;
       text_X1 += 800;
     }
+
+    // Wipe Screen to get to next scene
+    if (frameCount >= 360) {
+      background(255);
+      textSize(50);
+      textAlign(CENTER, CENTER);
+      text("LEAVING THE HOUSE... BE PATIENT OK", width / 2, height / 2);
+    }
   } 
 
   public void cutscene2() {
+    // Draw the new background, which is an image
     if (frameCount >= 540) {
       image(imgPathway, 0, 0);
       image(imgDadMedium, imgDadMedium_X, imgDadMedium_Y);
     }
   
+    // Adds a text box that gives a player directions on what to do
     if (frameCount >= 660) {
       fill(255, 25, 152);
       rect(175, 630, 850, 50);
@@ -133,6 +137,8 @@ public class Sketch1 extends PApplet {
       text("which path should I take? Hint: press Left/Right arrow Keys", text_X2, text_Y2);
     }
 
+    // If user presses LEFT arrow key, the character will move along the left path.  
+    // Collision detection is added in the draw method to see which path the character takes.
     if (frameCount >= 660) {
       if (keyCode == LEFT) {
         imgDadMedium_X -= 5;
@@ -140,6 +146,7 @@ public class Sketch1 extends PApplet {
       }
     }
     
+    // If user presses RIGHT arrow key, the character will move along the right path.
     if (frameCount >= 660) {
       if (keyCode == RIGHT) {
         imgDadMedium_X += 5;
@@ -150,32 +157,61 @@ public class Sketch1 extends PApplet {
   }
 
   public void cutscene3() {
+    // Add a white loading screen that has a message
     if (frameCount >= 720) {
       background(255);
       textSize(50);
       textAlign(CENTER, CENTER);
-      text("YOU TOOK THE WRONG PATH ...", width / 2, height / 2);
+      text("GOOD CHOICE ...", width / 2, height / 2);
+      text("HEH HEH", width / 2, 420);
     }
     
+    // Series of nested if statements that will carry out code based on the frame count.
+    // Draw the new background image, and draw the Dad
     if (frameCount >= 960) {
       image(imgDaWey, 0, 0);
       image(imgDadMedium, 1000, 500);
+      
+      // Add dialogue that the Dad says
+      if (frameCount >= 1040) {
+        fill(0);
+        textSize(30);
+        text("woah what is this place?", text_X3, text_Y3);
+      
+        // Draw Ugandan Knuckles image
+        if (frameCount >= 1260) {
+          imgUgandanKnuckles.resize(200, 163);
+          image(imgUgandanKnuckles, 510, 400);
 
-      if (frameCount >= 1100) {
-        imgUgandanKnuckles.resize(280, 263);
-        image(imgUgandanKnuckles, 450, 350);
-
-        if (frameCount >= 1160) {
-          image(imgDaWey, 0, 0);
-          imgDadMad.resize(150, 150);
-          image(imgUgandanKnuckles, 450, 350);
-          image(imgDadMad, 1000, 500);
-
-          if (frameCount >= 1340) {
+          // Redraw the background to refresh the scene. Draw in Dad with angry face, redraw Ugandan Knuckles, add new dialogue.
+          if (frameCount >= 1300) {
             image(imgDaWey, 0, 0);
-            imgDadDead.resize(150,150);
-            image(imgUgandanKnuckles, 450, 350);
-            image(imgDadDead, 1000, 500);
+            imgDadMad.resize(150, 150);
+            image(imgUgandanKnuckles, 510, 400);
+            image(imgDadMad, 1000, 500);
+
+            text("who the hell is u?", text_X3, text_Y3);
+
+            // Redraw to refresh the scene.  Draw in Dad Dead image, and add new dialogue.
+            if (frameCount >= 1540) {
+              image(imgDaWey, 0, 0);
+              imgDadDead.resize(150,150);
+              image(imgUgandanKnuckles, 510, 400);
+              image(imgDadDead, 1000, 500);
+              text("ouch", text_X3, text_Y3);
+
+              if (frameCount >= 1660) {
+                background(255);
+                textSize(50);
+                textAlign(CENTER, CENTER);
+                text("YOU DIED... WOMP WOMP", width / 2, 250);
+
+                textSize(30);
+                text("YOUR BRAIN SHUT DOWN BECAUSE", width/2, 400);
+                text("YOU COULD NOT COMPREHEND WHAT YOU JUST SAW", width/2, 450);
+                text("YOU DIED OF STROKE", width/2, 500);
+              }
+            }
           }
         }
       }
